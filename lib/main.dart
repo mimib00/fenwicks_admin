2fc7170bdc97e594cbbc6a37240acc22c4ed2059
Mohamed Malkia <mmimib001@gmail.com>
2022-09-19 16:29:21 +0100

    added notifications to the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,20 @@ import 'package:fenwicks_admin/firebase_options.dart';
 import 'package:fenwicks_admin/views/auth/controller/auth_controller.dart';
 import 'package:fenwicks_admin/views/root/controllers/navigation_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   Get.put(AuthController(), permanent: true);
   Get.put(NavigationController(), permanent: true);
   runApp(const MyApp());
--- lib/views/notifications/controller/notification_controller.dart
+++ lib/views/notifications/controller/notification_controller.dart
@@ -1,8 +1,11 @@
 import 'package:fenwicks_admin/meta/widgets/loading.dart';
 import 'package:fenwicks_admin/meta/widgets/snack_bar.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart' as material;
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationController extends GetxController {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -36,10 +39,10 @@ class NotificationController extends GetxController {
             style: TextStyle(
               fontSize: 18,
               fontFamily: "Baloo",
-              color: Colors.white,
+              color: material.Colors.white,
             ),
           ),
-          backgroundColor: Colors.green,
+          backgroundColor: material.Colors.green,
         ),
       );
       title.clear();
@@ -49,5 +52,105 @@ class NotificationController extends GetxController {
       Get.back();
       Get.showSnackbar(errorCard(e.toString()));
     }
+  }
+
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  // final CollectionReference<Map<String, dynamic>> _ref = FirebaseFirestore.instance.collection("orders");
+
+  final AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.high,
+  );
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  void onDidReceiveLocalNotification(int id, String? title, String? body, String? payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    Get.dialog(
+      CupertinoAlertDialog(
+        title: Text(title ?? ""),
+        content: Text(body ?? ""),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: const Text('Ok'),
+            onPressed: () async {},
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void onInit() async {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    //
+    const initialzationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    final initializationSettings = InitializationSettings(
+      android: initialzationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    //
+    await _messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    //
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      AppleNotification? apple = message.notification?.apple;
+
+      if (notification != null && android != null && apple != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              icon: "@mipmap/ic_launcher",
+            ),
+            iOS: const IOSNotificationDetails(),
+          ),
+        );
+      }
+    });
+
+    //
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      AppleNotification? apple = message.notification?.apple;
+      if (notification != null && android != null && apple != null) {
+        Get.showSnackbar(
+          GetSnackBar(
+            title: notification.title,
+            message: notification.body,
+            snackPosition: SnackPosition.TOP,
+            duration: const Duration(seconds: 3),
+          ),
+        );
+      }
+    });
+
+    //
+    super.onInit();
   }
 }
